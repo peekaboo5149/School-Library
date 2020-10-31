862de0c60304c57d166788103a8e2a5f1f1e206f
peekaboo5149 <ktanmay5149@gmail.com>
2020-10-31 13:43:17 +0530

    Update many things

--- lib/provider/edited_text.dart
+++ lib/provider/edited_text.dart
@@ -5,15 +5,16 @@ class EditedText with ChangeNotifier {
   String _name;
   String _roll;
   String _department;
-  bool gender;
+  bool _gender;
   bool theme;
 
   savePrefrence() async {
     var prefs = await SharedPreferences.getInstance();
+
     prefs.setString('name', _name);
     prefs.setString('roll', _roll);
     prefs.setString('dep', _department);
-    prefs.setBool('gender', gender);
+    prefs.setBool('gender', _gender);
   }
 
   loadPrefrence() async {
@@ -21,45 +22,44 @@ class EditedText with ChangeNotifier {
     String name = prefs.getString('name');
     String roll = prefs.getString('roll');
     String department = prefs.getString('dep');
-    bool bgender = prefs.getBool('gender');
+    bool gender = prefs.getBool('gender');
 
     if (_name == "Default Name") setName(name);
     if (_roll == "0") setRoll(roll);
     if (_department == "department") setDepartment(department);
-    if (gender == false) {
-      this.gender = bgender;
-    }
+    if (_gender == false) genderSetter(gender);
   }
 
   EditedText() {
     _name = "Default Name";
     _roll = "0";
     _department = "department";
-    gender = false;
+    _gender = false;
     theme = false;
-    // loadPrefrence();
+    loadPrefrence();
   }
 
   String get name => _name;
   String get roll => _roll;
   String get department => _department;
-  // bool get gender => gender;
+  bool get gender => _gender;
 
   void setName(String name) {
     _name = name;
     notifyListeners();
-    // savePrefrence();
+    savePrefrence();
   }
 
   void setRoll(String roll) {
     _roll = roll;
     notifyListeners();
+    savePrefrence();
   }
 
   void setDepartment(String department) {
     _department = department;
     notifyListeners();
-    
+    savePrefrence();
   }
 
   void setTheme(bool value) {
@@ -67,8 +67,15 @@ class EditedText with ChangeNotifier {
     notifyListeners();
   }
 
-  void setGender() {
-    gender = !gender;
+  void genderSetter(bool gender) {
+    _gender = gender;
     notifyListeners();
+    savePrefrence();
+  }
+
+  void setGender() {
+    _gender = !_gender;
+    notifyListeners();
+    savePrefrence();
   }
 }
